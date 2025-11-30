9f9e667e863775a435834eb7a2fe52be2dac39dd
Daniel Loera <169018139+Bullsquidz@users.noreply.github.com>
2025-11-29 18:34:13 -0600

    Update Referee.ino

--- Referee.ino
+++ Referee.ino
@@ -1,9 +1,43 @@
-void setup() {
-  // put your setup code here, to run once:
+// C++ code
+//
 
+enum GameState{  
+  IDLE,
+  PREPARE, //Select between rock paper or scissors
+  RESULT
+};
+
+
+const int twoPlayer = 6;
+bool gameStarted = false;
+
+String playerOne = "?";
+String playerTwo = "?";
+String computer  = "?";
+
+void setup()
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+  	Serial.begin(9600);
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+int oldMils, newMils = -1;
+void loop(){
+  	if (digitalRead(twoPlayer) && !gameStarted){
+  		Serial.write("start\n");
+  		gameStarted = true;
+    	oldMils = millis();
+  	}
+  
+  	while(gameStarted){
+  		newMils = millis();
+      	if (newMils - oldMils > 300){
+    		Serial.write("stop\n");
+      		gameStarted = false;
+	    }
+      
+      	
+  	}
+  
 }
